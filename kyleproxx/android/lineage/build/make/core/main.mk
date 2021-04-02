4f2577069d9b74a76f4db90e52aef4b7f95b2799
David <davidmolinamorales1999@gmail.com>
2021-04-02 15:32:05 +0200

    Update main.mk

--- kyleproxx/android/lineage/build/make/core/main.mk
+++ kyleproxx/android/lineage/build/make/core/main.mk
@@ -161,8 +161,8 @@ $(info You asked for an OpenJDK based build but your version is)
  $(info $(java_version_str).)
  $(info ************************************************************)
  #$(error stop)
- #endif # java version is not OpenJdk
- #else # if requires_openjdk
+ endif # java version is not OpenJdk
+ else # if requires_openjdk
  ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
  
 
